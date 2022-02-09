829efc1533a3725e9466beb11a3e303813b0ecb3
amandine carreno <30933638+carra1358@users.noreply.github.com>
2022-02-09 15:51:50 +0100

    confing
    mise en place des routes principales, ajoutes des assets et data

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
+
 import './styles/home.css';
 import Home from './pages/Home';
+import About from "./pages/About";
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="about" element={<About/>}/>
+      </Routes>
+      
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
